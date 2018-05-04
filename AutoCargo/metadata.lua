7f55618194edab07a08e634012832f254dd02719
machaqueso <machaqueso@gmail.com>
2018-05-04 00:33:44 -0500

    Updated metadata

--- AutoCargo/metadata.lua
+++ AutoCargo/metadata.lua
@@ -1,6 +1,6 @@
 return PlaceObj('ModDef', {
 	'title', "AutoCargo",
-	'description', "Enables your Transport Rovers to automatically move cargo between resource depots.",
+	'description', "Enables your Transport Rovers to automatically haul resources between depots.\r\n\r\nNote: This is a work in progress by a LUA noob, will continue to improve.\r\n\r\nLatest Changes: replaced my potato haul task finding algorithm with the one used by Shuttle Hubs with some tweaks to prioritize empty depots.\r\n\r\nSource code in github: https://github.com/machaqueso/surviving-mars",
 	'image', "AutoCargo_preview.png",
 	'id', "uo54uro",
 	'steam_id', "1368419943",
